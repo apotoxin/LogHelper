3bcd5ad519aa7a2e6324e5fa702380176aa76cb5
mixiaochuan <mixiaochuan@163.com>
2016-12-04 11:57:32 +0800

    Add Original code
    Signed-off-by: mixiaochuan <mixiaochuan@163.com>

---
+++ LogHelper/app/src/main/java/loghelper/workshop/ami/loghelper/LogHelperActivity.java
@@ -0,0 +1,13 @@
+package loghelper.workshop.ami.loghelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogHelperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_helper);
+    }
+}
